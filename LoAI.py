b779a5df47811830f2a9db5d1ca84b6975456b82
Juwon Kwon <juwonkwon.dev@gmail.com>
2021-08-25 02:32:39 +0900

    원정대 정보 조회 추가

---
+++ LoAI.py
@@ -0,0 +1,31 @@
+import os
+from urllib.request import urlopen
+from urllib import parse
+from bs4 import BeautifulSoup
+
+class Character():
+    def __init__(self, name):
+        self.name = name
+        
+        self.profile_html = urlopen(f"https://lostark.game.onstove.com/Profile/Character/{parse.quote(self.name)}")
+        self.profile = BeautifulSoup(self.profile_html, "html.parser")
+        
+        self.level = str(self.profile.select("#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info > div.level-info__item > span"))[45:-8]
+        self.char = str(self.profile.select("#lostark-wrapper > div > main > div > div.profile-ingame > div.profile-info > div.level-info2 > div.level-info2__expedition"))[84:]
+        self.target_idx = self.char.find("<small>")
+        self.item_level = self.char[:self.target_idx] + self.char[self.target_idx+7:self.target_idx+10]
+        print(self.name, self.level, self.item_level)
+
+class Expadition():
+    def __init__(self, name):
+        self.profile_html = urlopen(f"https://lostark.game.onstove.com/Profile/Character/{parse.quote(name)}")
+        self.profile = BeautifulSoup(self.profile_html, "html.parser")
+        self.char = list(self.profile.select("#expand-character-list > ul > li > span > button > span"))
+
+        self.character = [] 
+        for i in self.char:
+            self.character.append(Character(str(i)[6:-7]))
+
+if __name__ == '__main__':
+    name = "행복관NPC"
+    expadition = Expadition(name)
