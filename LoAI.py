2afb1a633c1326e2ee2090661eef6cec86f4545f
Juwon Kwon <juwonkwon.dev@gmail.com>
2021-08-30 11:38:12 +0900

    불러오기 시점 todo로 변경

--- LoAI.py
+++ LoAI.py
@@ -1,8 +1,6 @@
 import sys
-import os
-import json
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QFont, QFontDatabase 
+from PyQt5.QtGui import QFont, QFontDatabase
 
 from mainWindow import MainWindow
 from expaditionData import Expadition
@@ -12,13 +10,5 @@ if __name__ == '__main__':
     font = QFontDatabase()
     font.addApplicationFont("./font/NanumBarunGothic.ttf")
     app.setFont(QFont("NanumBarunGothic"))
-    #<로딩화면>
-
-    # 저장된 원정대 정보 확인
-    expadition = []
-    with open("preference.json", "rt", encoding="UTF-8") as file:
-        config = json.load(file)
-    if config["account_count"] != 0:
-        expadition = [Expadition(i, config["account_character"][i]) for i in range(config["account_count"])]
-    setting_window = MainWindow(expadition)
+    setting_window = MainWindow()
     sys.exit(app.exec_())
