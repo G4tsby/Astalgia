74e06ecee0076a0c983f7ae68e43f94932bce293
Juwon Kwon <juwonkwon.dev@gmail.com>
2021-11-16 15:20:32 +0900

    style(SideBar,TopBar): 클래스 구현 방식 변경

--- sideBar.py
+++ sideBar.py
@@ -1,15 +1,51 @@
-from PySide6.QtWidgets import QLabel, QWidget, QGraphicsOpacityEffect
+from PySide6.QtWidgets import QLabel, QPushButton, QWidget, QGraphicsOpacityEffect
 
-def SideBar(parent):
-    side_bar = QWidget(parent)
-    side_bar.setGeometry(0, 65, 200, 655)
-    side_bar.setStyleSheet("background: none")
 
-    alpha = QGraphicsOpacityEffect(parent)
-    alpha.setOpacity(0.24)
-    side_bar.backgound = QWidget(parent)
-    side_bar.backgound.setGeometry(0, 65, 200, 655)
-    side_bar.backgound.setStyleSheet("background: #323238")
-    side_bar.backgound.setGraphicsEffect(alpha)
+class SideBar(QWidget):
+    def __init__(self, par):
+        super().__init__(par)
+        self.setGeometry(0, 65, 200, 655)
+        self.setStyleSheet("background: none")
+        alpha = QGraphicsOpacityEffect(par)
+        alpha.setOpacity(0.24)
+        background = QWidget(par)
+        background.setGeometry(0, 65, 200, 655)
+        background.setStyleSheet("background: #323238")
+        background.setGraphicsEffect(alpha)
 
-    return side_bar
+        # 할일
+        todo_text = QLabel("할일", par)
+        todo_text.setStyleSheet("background: none; color: #ffffff; font-size: 14pt")
+        todo_text.move(32, 204)
+
+        account_button = []
+        for i in range(len(par.account)):
+            name = par.account[i].character[0].name
+            if len(name) > 8:
+                name = name[:6] + "..."
+                width = (len(name)-2) * 15 + 22
+            else:
+                width = len(name) * 15 + 22
+            account_button.append(QPushButton(name, par))
+            account_button[i].setGeometry(40, 234 + i * 30, width, 30)
+            account_button[i].move(40, 234 + i * 30)
+            account_button[i].setStyleSheet("""
+                QPushButton {
+                    background: rgba(0,0,0,0);
+                    color: #ffffff;
+                    font-size: 12pt;
+                    text-align: left;
+                    padding-left: 10px;
+                }
+                QPushButton:hover {
+                    background: rgba(255, 255, 255, 30);
+                    border-radius: 3px;
+                }
+                """)
+        # 오버레이
+        # 파티모집 템세팅 확인
+        # 돌파고
+        # 보스 패턴 알림
+        # 계산기
+        # 각인 계산기
+        # 생활 계산기
--- topBar.py
+++ topBar.py
@@ -1,23 +1,24 @@
 from PySide6.QtWidgets import QWidget, QPushButton, QLabel
 from PySide6.QtCore import QCoreApplication
 
-def TopBar(parent):
-    top_bar = QWidget(parent)
-    top_bar.setGeometry(0, 0, 1280, 65)
-    top_bar.setStyleSheet("background: none; border-bottom: 1px solid rgba(110, 110, 110, 0.15);")
+class TopBar(QWidget):
+    def __init__(self, par):
+        super().__init__(par)
+        top_bar = QWidget(par)
+        top_bar.setGeometry(0, 0, 1280, 65)
+        top_bar.setStyleSheet("background: none; border-bottom: 1px solid rgba(110, 110, 110, 0.15);")
 
-    # 로고
-    logo = QWidget(parent)
-    logo.setGeometry(17, 11, 40, 43)
-    logo.setStyleSheet("border-image: url(./image/4nem_mini.png); background: none;")
-    logo_text = QLabel("Astalgia", parent)
-    logo_text.setGeometry(70, 15, 150, 35)
-    logo_text.setStyleSheet("font-size: 18pt; color: #abb1f4; background: none;")
+        # 로고
+        logo = QWidget(par)
+        logo.setGeometry(17, 11, 40, 43)
+        logo.setStyleSheet("border-image: url(./image/4nem_mini.png); background: none;")
+        logo_text = QLabel("Astalgia", par)
+        logo_text.setGeometry(70, 15, 150, 35)
+        logo_text.setStyleSheet("font-size: 18pt; color: #abb1f4; background: none;")
 
-
-    # X 버튼
-    top_bar.exit_button = QPushButton(parent)
-    top_bar.exit_button.setStyleSheet(
+        # X 버튼
+        top_bar.exit_button = QPushButton(par)
+        top_bar.exit_button.setStyleSheet(
             """
             QPushButton {
                 border-image: url(./image/exit.png);
@@ -26,29 +27,29 @@ def TopBar(parent):
                 border-image: url(./image/exit_p.png);
             }
             """)
-    top_bar.exit_button.setGeometry(1215, 15, 35, 35)
-    top_bar.exit_button.clicked.connect(QCoreApplication.instance().quit)
+        top_bar.exit_button.setGeometry(1215, 15, 35, 35)
+        top_bar.exit_button.clicked.connect(QCoreApplication.instance().quit)
 
-    # ㅁ 버튼
-    top_bar.max_button = QPushButton(top_bar)
-    top_bar.max_button.setStyleSheet(
-        """
-        QPushButton {
-            border-image: url(./image/max.png);
-        }
-        """)
-    top_bar.max_button.setGeometry(1180, 15, 35, 35)
+        # ㅁ 버튼
+        top_bar.max_button = QPushButton(top_bar)
+        top_bar.max_button.setStyleSheet(
+            """
+            QPushButton {
+                border-image: url(./image/max.png);
+            }
+            """)
+        top_bar.max_button.setGeometry(1180, 15, 35, 35)
 
-    # _ 버튼
-    top_bar.min_button = QPushButton(top_bar)
-    top_bar.min_button.setStyleSheet(
-        """
-        QPushButton {
-            border-image: url(./image/min.png);
-        }
-        QPushButton:hover {
-            border-image: url(./image/min_p.png);
-        }
-        """)
-    top_bar.min_button.setGeometry(1145, 15, 35, 35)
-    top_bar.min_button.clicked.connect(parent.showMinimized)
+        # _ 버튼
+        top_bar.min_button = QPushButton(top_bar)
+        top_bar.min_button.setStyleSheet(
+            """
+            QPushButton {
+                border-image: url(./image/min.png);
+            }
+            QPushButton:hover {
+                border-image: url(./image/min_p.png);
+            }
+            """)
+        top_bar.min_button.setGeometry(1145, 15, 35, 35)
+        top_bar.min_button.clicked.connect(par.showMinimized)
